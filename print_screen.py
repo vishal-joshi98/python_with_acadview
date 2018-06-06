9f628982e1afeb37d81f7f40ad0e12636ca0a8ac
vishal joshi <31546600+vishu1998@users.noreply.github.com>
2018-06-06 10:54:06 +0530

    Add files via upload

---
+++ print_screen.py
@@ -0,0 +1,5 @@
+#question 4: printing on screen
+def printing2():
+      print("let's gert started")
+
+printing2()
---
+++ printing.py
@@ -0,0 +1,5 @@
+#question 1:printing anything
+def printing():
+      print("start learning python with acadview")
+
+printing()
