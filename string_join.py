ae903132b58c3c5a836605a1464a2124b3a230ef
vishal joshi <31546600+vishu1998@users.noreply.github.com>
2018-06-07 18:13:24 +0530

    Delete string_join.py

--- string_join.py
+++
@@ -1,7 +0,0 @@
-#question 2 : two string join
-def join(str1,str2):
-      print("after join string:",str1+str2)
-
-str1=input("enter first string:")
-str2=input("enter the second string:")
-join(str1,str2)
