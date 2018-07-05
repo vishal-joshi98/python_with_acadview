3127e45c953ac5d60e99b1bb3324ed65d5cd2619
Vishal Joshi <vi11611275@gmail.com>
2018-07-06 01:02:40 +0530

    27th_june

--- 27th_june_assignment/que2.py
+++ 27th_june_assignment/que2.py
@@ -1,21 +1,27 @@
 dic = {}
-def printtext():
-    global e
 
-    string = e.get()
-    print(string)
+
+def item_insert():
+    global key ,value
+    k = key.get()
+    v = value.get()
+
+    dic[k]=v
+    print(dic)
 
 from tkinter import *
+
 root = Tk()
 
 root.title('ITEM')
-Message(root, text= "ENTER KEY VALUE").pack()
+Label(root, text="ENTER KEY AND VALUE").grid(row=3,column=1)
+Label(root, text="key").grid(row=0)
+Label(root , text="value").grid(row=1)
+key = Entry(root)
+value = Entry(root)
+key.grid(row=0,column=1)
+value.grid(row=1, column=1)
 
-e = Entry(root)
-e.pack()
-e.focus_set()
 
-b = Button(root,text='SUBMIT',command=printtext)
-b.pack(side='bottom')
-print(dic)
+b = Button(root, text='SUBMIT', command=item_insert).grid(row=2,column=1)
 root.mainloop()
